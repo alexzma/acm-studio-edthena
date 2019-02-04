1041c52bf6b4ebdb16a4eef78d4ae8e4ae188eb4
Alexander Ma <alexanderzma@hotmail.com>
2019-02-03 19:08:45 -0800

    Bsic Platformer With a Cube

---
+++ Assets/CubeController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeController : MonoBehaviour
+{
+    public float speed = 100;
+    public float jumpSpeed = 250;
+    private Rigidbody rb;
+
+    // Use this for initialization
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+        float y = 0;
+        rb.AddForce(new Vector3(x - z, 0, 0) * speed * Time.deltaTime);
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            y = 1;
+        }
+        rb.AddForce(new Vector3(0, y, 0) * jumpSpeed * Time.deltaTime, ForceMode.Impulse);
+    }
+}
---
+++ Assets/Reset.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        Rigidbody rb = other.GetComponent<Rigidbody>();
+        rb.transform.position = new Vector3((float)0.41, (float)-1.08, (float)-1.844946);
+        rb.velocity = Vector3.zero;
+    }
+}
