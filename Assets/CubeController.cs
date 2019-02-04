f23c63883380250180e5289214709fc4e3126edd
Alexander Ma <alexanderzma@hotmail.com>
2019-02-03 20:38:09 -0800

    Cube Rolling
    Made the cube roll so you wouldn't have to jump to move

--- Assets/CubeController.cs
+++ Assets/CubeController.cs
@@ -6,6 +6,7 @@ public class CubeController : MonoBehaviour
 {
     public float speed = 100;
     public float jumpSpeed = 250;
+    public float sidespin = 100;
     public bool jumping = false;
     private Rigidbody rb;
 
@@ -22,12 +23,16 @@ public class CubeController : MonoBehaviour
         float z = Input.GetAxis("Vertical");
         float y = 0;
         rb.AddForce(new Vector3(x - z, 0, 0) * speed * Time.deltaTime);
-        if (Input.GetKeyDown(KeyCode.Space) && !jumping)
+        if (!jumping)
         {
-            y = 1;
-            jumping = true;
+            rb.AddTorque(new Vector3(0, 0, z-x) * sidespin * Time.deltaTime, ForceMode.Acceleration);
+            if (Input.GetKeyDown(KeyCode.Space) || Input.GetKeyDown(KeyCode.UpArrow))
+            {
+                y = 1;
+                jumping = true;
+                rb.AddForce(new Vector3(0, y, 0) * jumpSpeed * Time.deltaTime, ForceMode.Impulse);
+            }
         }
-        rb.AddForce(new Vector3(0, y, 0) * jumpSpeed * Time.deltaTime, ForceMode.Impulse);
     }
     private void OnCollisionEnter(Collision collision)
     {
