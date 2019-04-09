9e58ab69fb743c6171840e5268af3cd6290708ca
alexzma <alexanderzma@hotmail.com>
2019-04-08 17:56:38 -0700

    No Infinitely Looping Music

---
+++ Assets/Scripts/DontDestroyOnLoad.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DontDestroyOnLoad : MonoBehaviour
+{
+
+    public static DontDestroyOnLoad Instance;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void Awake()
+    {
+        if (Instance != null)
+        {
+            GameObject.Destroy(Instance);
+        }
+        else
+        {
+            Instance = this;
+            DontDestroyOnLoad(this);
+        }
+    }
+}
